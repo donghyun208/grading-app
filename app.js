887da906948c7a3da772065dddc35168a169cb76
Donghyun <donghyun208@gmail.com>
2014-02-25 17:35:36 -0800

    update port

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 // var server = http.createServer(app);
 var server = https.createServer(options, app);
 
-app.set('port', 8080);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
